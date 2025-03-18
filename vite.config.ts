b3b881425a180b3ce38f4e229bf38ab512e2025b
Vlekzander <vlekzander@nomail.com>
2025-03-18 12:06:12 +0100

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+    plugins: [
+        tailwindcss(),
+    ],
+    server: {
+        watch: {
+            usePolling: true,
+        },
+    },
+    build: {
+        rollupOptions: {
+            input: {
+                main: 'index.html'
+            }
+        }
+    }
+})
