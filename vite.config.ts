dd7b3236c9090d183d378840157fc7a0725e03d4
Vlekzander <alexandrepierret@protonmail.com>
2025-04-07 13:06:38 +0200

    Skip project 8

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,6 @@ export default defineConfig({
                 project1: 'projects/project-1/index.html',
                 project2: 'projects/project-2/index.html',
                 project3: 'projects/project-3/index.html',
-                project4: 'projects/project-4/index.html',
                 project5: 'projects/project-5/index.html',
                 project6: 'projects/project-6/index.html',
                 project7: 'projects/project-7/index.html',
